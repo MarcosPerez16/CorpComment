8dfe633168e67c058f04c2cb1e496f4aace811ed
MarcosPerez16 <marcos.perez06@yahoo.com>
2025-08-04 19:22:11 -0700

    Created the counter logic for the submit form and useState to control input

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 const FeedbackForm = () => {
+  const [text, setText] = useState("");
+
+  const characterCount = MAX_CHARACTERS - text.length;
+
   return (
-    <form className='form'>
-      <textarea id='feedback-textarea' placeholder='test' spellCheck={false} />
-      <label htmlFor='feedback-textarea'>
+    <form className="form">
+      <textarea
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        id="feedback-textarea"
+        placeholder="test"
+        spellCheck={false}
+      />
+      <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className='u-italic'>150</p>
+        <p className="u-italic">{characterCount}</p>
         <button>
           <span>Submit</span>
         </button>
