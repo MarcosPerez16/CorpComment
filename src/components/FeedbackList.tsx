e5cbe85d081242b0a9553c860d5efab839065068
MarcosPerez16 <marcos.perez06@yahoo.com>
2025-08-05 20:37:36 -0700

    Create an async await function inside our useEffect to fetch actual data for our list. Created a loading spinner for when data is being fetched for better user experience. Created error state to handle errors. There are two errors in the files that still need to be fixed

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,27 +1,46 @@
-import { TriangleUpIcon } from '@radix-ui/react-icons';
+import { useEffect, useState } from "react";
+import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 const FeedbackList = () => {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        setErrorMessage("Something went wrong. Please try again later.");
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchFeedbackItems();
+  }, []);
+
   return (
-    <ol className='feedback-list'>
-      <li className='feedback'>
-        <button>
-          <TriangleUpIcon />
-          <span>593</span>
-        </button>
-        <div>
-          <p>B</p>
-        </div>
+    <ol className="feedback-list">
+      {isLoading && <Spinner />}
 
-        <div>
-          <p>ByteGrad</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
-            totam dolores et doloribus sint ipsam.
-          </p>
-        </div>
-
-        <p>4d</p>
-      </li>
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+      {feedbackItems.map((feedbackItem) => (
+        <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
+      ))}
     </ol>
   );
 };
